c1bf22bb1766e2c427c641ad003275f8140da1f4
kusoinkyaa <youxijusen@gmail.com>
2024-09-20 04:50:26 +0000

    9月19日分を別ファイルに移動

---
+++ 9_19/main.js
@@ -0,0 +1,17 @@
+document.querySelector("h1").textContent = "きくもり"
+document.querySelector("h2").textContent = "ゆずき"
+
+//documentとは
+/*htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。*/
+
+//DOM(ドム)
+/*ドキュメントオブジェクトモデル　(Document Object Mode)の略で、webページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
+
+//上記コードの意味
+//document.queryselector("h1").textContent = "自分の名前"
+
+//ドキュメントの中の("h1")タグを探してね
+//document.querySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent = "自分の名前"
